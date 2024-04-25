af72cdecaead25fb947f565406aedafb4dbc5b92
sayuveos11 <kkallinberg@gmail.com>
2024-04-25 12:22:06 +0300

    Добавьте файлы проекта.

---
+++ lab 20/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace lab_20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Side1 = new System.Windows.Forms.TextBox();
+            this.Side3 = new System.Windows.Forms.TextBox();
+            this.Side2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Side1
+            // 
+            this.Side1.Location = new System.Drawing.Point(148, 62);
+            this.Side1.Name = "Side1";
+            this.Side1.Size = new System.Drawing.Size(117, 20);
+            this.Side1.TabIndex = 0;
+            // 
+            // Side3
+            // 
+            this.Side3.Location = new System.Drawing.Point(148, 137);
+            this.Side3.Name = "Side3";
+            this.Side3.Size = new System.Drawing.Size(117, 20);
+            this.Side3.TabIndex = 1;
+            // 
+            // Side2
+            // 
+            this.Side2.Location = new System.Drawing.Point(148, 100);
+            this.Side2.Name = "Side2";
+            this.Side2.Size = new System.Drawing.Size(117, 20);
+            this.Side2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(51, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Сторона 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(51, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Сторона 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(51, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Сторона 3:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnCalculate.Location = new System.Drawing.Point(55, 190);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(172, 64);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Розрахунок";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Side2);
+            this.Controls.Add(this.Side3);
+            this.Controls.Add(this.Side1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Side1;
+        private System.Windows.Forms.TextBox Side3;
+        private System.Windows.Forms.TextBox Side2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ lab 20/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Отримання значень сторін з текстових полів
+                double side1 = double.Parse(Side1.Text);
+                double side2 = double.Parse(Side2.Text);
+                double side3 = double.Parse(Side3.Text);
+
+                // Перевірка чи сторони утворюють трикутник
+                if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+                {
+                    // Обчислення периметра
+                    double perimeter = side1 + side2 + side3;
+                    MessageBox.Show($"Периметр трикутника: {perimeter}");
+                }
+                else
+                {
+                    MessageBox.Show("Введені значення не утворюють трикутник.");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введено некоректне значення. Будь ласка, введіть число.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Сталася помилка: {ex.Message}");
+            }
+        }
+    }
+}
